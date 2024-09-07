dba10ac4e1f1214ed2c1378ac161f006d0c95492
kripesh mainali <139145250+kripeshAlt@users.noreply.github.com>
2024-09-07 12:09:06 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
-from javascript import require, On, Once, AsyncTask, once, off
+const mineflayer = require('mineflayer');
 
-# Import the javascript libraries
-mineflayer = require("mineflayer")
+const botArgs = {
+    host: 'KRIPESH007.aternos.me',
+    port: '29015',
+    username: "Hello_world",
+    version: '1.20.1'
+};
 
-# Create bot
-bot_args = {"username": "reconnect-bot", "host": "localhost", "port": 3000, "version": "1.19.4", "hideErrors": False}
+const initBot = () => {
 
-reconnect = True
+    // Setup bot connection
+    let bot = mineflayer.createBot(botArgs);
 
-def start_bot():
-    bot = mineflayer.createBot(bot_args)
+    bot.on('login', () => {
+        let botSocket = bot._client.socket;
+        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
+    });
 
-    # Login event (Logged in)
-    @On(bot, "login")
-    def login(this):
-        bot_socket = bot._client.socket
-        print(
-            f"Logged in to {bot_socket.server if bot_socket.server else bot_socket._host }"
-        )
+    bot.on('end', () => {
+        console.log(`Disconnected`);
 
-    # Kicked event (Got kicked from server)
-    @On(bot, "kicked")
-    def kicked(this, reason, loggedIn):
-        if loggedIn:
-            print(f"Kicked whilst trying to connect: {reason}")
+        // Attempt to reconnect
+        setTimeout(initBot, 5000);
+    });
 
-    # Chat event (Received message in chat)
-    @On(bot, "messagestr")
-    def messagestr(this, message, messagePosition, jsonMsg, sender, verified):
-        if messagePosition == "chat" and "quit" in message:
-            global reconnect
-            reconnect = False
-            this.quit()
+    bot.on('error', (err) => {
+        if (err.code === 'ECONNREFUSED') {
+            console.log(`Failed to connect to ${err.address}:${err.port}`)
+        }
+        else {
+            console.log(`Unhandled error: ${err}`);
+        }
+    });
+};
 
-    # End event (Disconnected from server)
-    @On(bot, "end")
-    def end(this, reason):
-        print(f"Disconnected: {reason}")
-        off(bot, "login", login)
-        off(bot, "kicked", kicked)
-        off(bot, "messagestr", messagestr)
-        if reconnect:
-            print("RESTARTING BOT")
-            start_bot()
-        off(bot, "end", end)
-
-
-# Run function that starts the bot
-start_bot()
+initBot();
